303d6703a8929311050e788aaa40a4de9172e280
iiiivaska <iiiivaska@yandex.ru>
2021-09-25 18:24:29 +0300

    Initial Commit

---
+++ ViewsAndmodifiers/ViewsAndmodifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndmodifiersApp.swift
+//  ViewsAndmodifiers
+//
+//  Created by Василий Буланов on 9/25/21.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndmodifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
