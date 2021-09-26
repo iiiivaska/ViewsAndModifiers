135cf70b9c00d596ed25301ac6e5a2a367ec647b
iiiivaska <iiiivaska@yandex.ru>
2021-09-26 13:05:36 +0300

    Example for custom ViewModifier

--- ViewsAndmodifiers/ContentView.swift
+++ ViewsAndmodifiers/ContentView.swift
@@ -7,10 +7,24 @@
 
 import SwiftUI
 
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(.blue)
+            .font(.largeTitle)
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(Title())
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Title")
+            .titleStyle()
     }
 }
 
